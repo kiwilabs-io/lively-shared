130cf5ec0cc183cc345997f807f2f0c36a3a02f6
andrew <andrew@noyes.io>
2019-01-03 17:30:44 -0700

    removed host config

--- lib/config/host.config.ts
+++
@@ -1,6 +0,0 @@
-import { isProduction } from './env.config';
-
-if (isProduction) throw new Error('No production host URLs configured');
-
-export const webBaseUrl = 'localhost:3000';
-export const apiBaseUrl = 'http://localhost:1337';
